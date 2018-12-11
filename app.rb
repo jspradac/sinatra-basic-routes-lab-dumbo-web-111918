e74509fcc095bfe24c013c9edb85d3b63c7cd7c0
jspradac <jspradac@gmail.com>
2018-12-11 10:49:34 -0500

    Lab Completed

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is King Barfly"
+  end
+
+  get "/hometown" do
+    "My hometown is the land of shallow bones"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is 'The Kings\'s March'"
+
+  end
 end
